af477cb5c3aaf7cba6e2665b301145e78dc41bec
Erik Drouhard <ekdro23@gmail.com>
2019-11-03 21:20:31 -0500

    modifying our story

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -11,9 +11,11 @@ export default function OurStory() {
       <p>
         Becca and Erik found each other on OkCupid. Erik says “I came across
         this attractive woman who had ‘loves Worcester, MA’ in her profile and
-        just had to learn more–I didn’t know the city had die hard fans!” After
-        a few messages back and forth, the two decided to meet for a game of
-        mini golf—Becca won. Four years later at Peter Haven’s restaurant in
+        just had to learn more–I didn’t know the city had die hard fans!”.
+      </p>
+      <p>
+        After a few messages back and forth, the two decided to meet for a game
+        of mini golf—Becca won. Four years later at Peter Haven’s restaurant in
         Vermont, Erik proposed—she said yes. You’ll frequently find the two
         going on new adventures, hiking and trying to figure out what’s for
         dinner this week. They are very much looking forward to seeing everyone
