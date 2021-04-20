18280e5748ff930b026e7e6135d555a971adb050
Erik Drouhard <ekdro23@gmail.com>
2021-04-19 22:43:37 -0400

    removing abril fatface from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Abril Fatface`,
-            subsets: [`latin`],
-          },
-        ],
-      },
-    },
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
