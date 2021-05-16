56050eaa84d0b0ea2c8fceba428c112512df219d
Erik Drouhard <ekdro23@gmail.com>
2021-05-16 16:40:01 -0400

    removing css import

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import imgSrc from "../images/about-us.jpg"
 
 function OurStory() {
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 
 const CTAButton = styled.a`
   background: #9b51e0;
